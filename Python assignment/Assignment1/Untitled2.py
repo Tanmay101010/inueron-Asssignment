fcca4ed3ad062ba53a7e4bb65b2b04ff9153dcda
Tanmay Bhargava <101199050+Tanmay101010@users.noreply.github.com>
2022-11-30 20:57:45 +0530

    Add files via upload

---
+++ Python assignment/Assignment1/Untitled2.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+### Question1
+print("Hello World")
+
+
+# In[3]:
+
+
+#### Qurstion 2
+13+5
+
+
+# In[4]:
+
+
+13/5
+
+
+# In[5]:
+
+
+### Question3
+b = int(input("Enter Base"))
+h = int(input("Enter Height"))
+area = 1/2*b*h
+print("Area of Triangle is:", area)
+
+
+# In[7]:
+
+
+#### Question 4
+x = 13
+y = 12
+Temp = x
+print("Value of temp variable:",Temp)
+x = y
+print("Value of x:", x)
+y = Temp
+print("Value of y:", y)
+
+
+# In[11]:
+
+
+### Question 4
+import random
+num = random.randint(5,15)
+print(num)
+
